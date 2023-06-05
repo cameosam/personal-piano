c06ec898b419eeb471f24846b0d3689824c7a26c
cameo <cameosam@gmail.com>
2023-06-05 15:12:33 -0700

    update dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from './Header';
-import Footer from './Footer';
-import Game from './Game';
-import Freeplay from './Freeplay';
-import Learn from './Learn';
+import Header from "./Header";
+import Footer from "./Footer";
+import Game from "./Game";
+import Freeplay from "./Freeplay";
+import Learn from "./Learn";
 
 export default function App() {
-  return (  
+  return (
     <Router>
-      <Header/>
-      <Switch>
-        <Route path='/game'><Game/></Route> 
-        <Route path='/learn'><Learn/></Route> 
-        <Route path='/freeplay'><Freeplay/></Route> 
-        <Route path='/'><Freeplay/></Route> 
-      </Switch>
-      <p><span className='keyboard-color'>keyboard keys</span> | <span className='note-color'>piano notes</span></p>
-      <Footer/>
+      <Header />
+      <Routes>
+        <Route path="/game" element={<Game />} />
+        <Route path="/learn" element={<Learn />} />
+        <Route path="/freeplay" element={<Freeplay />} />
+        <Route path="/" element={<Freeplay />} />
+      </Routes>
+      <p>
+        <span className="keyboard-color">keyboard keys</span> |{" "}
+        <span className="note-color">piano notes</span>
+      </p>
+      <Footer />
     </Router>
   );
 }
