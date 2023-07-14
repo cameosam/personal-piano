8179af433ce906a007a221068d869eefe97dd7ed
cameo <cameosam@gmail.com>
2023-07-13 21:33:29 -0700

    Fix issue with buttons not being able to be selected

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Piano from "./Piano";
-import { ButtonGroup, ToggleButton } from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
 import songs from "../songs";
 import { animateButton } from "../feedback";
 
@@ -38,33 +38,29 @@ export default function Learn(props) {
 
   return (
     <div>
-      <div className="songs">
-        <ButtonGroup name="songs" toggle>
-          {songs.map((song, index) => (
-            <ToggleButton
-              key={index}
-              type="radio"
-              variant="secondary"
-              value={index}
-              checked={songNumber === index}
-              onChange={(e) => {
-                setNoteNumber(0);
-                setSongNumber(e.currentTarget.value);
-                setSubtitleLearn(
-                  "press " +
-                    songs[e.currentTarget.value].value[0] +
-                    " to start!"
-                );
-              }}
-            >
-              {song.name}
-            </ToggleButton>
-          ))}
-        </ButtonGroup>
-      </div>
       <h1>learn "{songs[songNumber].name}"</h1>
       <h2>{subtitleLearn}</h2>
       <Piano keySelect={keySelect} />
+      <ButtonGroup name="songs">
+        {songs.map((song, index) => (
+          <Button
+            key={index}
+            type="radio"
+            variant="white"
+            value={index}
+            checked={songNumber === index}
+            onClick={(e) => {
+              setNoteNumber(0);
+              setSongNumber(e.currentTarget.value);
+              setSubtitleLearn(
+                "press " + songs[e.currentTarget.value].value[0] + " to start!"
+              );
+            }}
+          >
+            {song.name}
+          </Button>
+        ))}
+      </ButtonGroup>
     </div>
   );
 }
